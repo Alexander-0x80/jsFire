35c7b462fff210eaeaadba8ef9138e0fd4bbb40f
rapt0r <rapt0r@bulldozer.(none)>
2013-02-16 18:40:12 +0200

    Source optimizations

--- js/fire.js
+++ js/fire.js
@@ -27,8 +27,7 @@
 
 
     /* Main Loop */
-    function render(){
-        
+    function render(){   
         var start = imageData.length - canvas.width*4;
         var oneLine = canvas.width*4;
 
@@ -40,28 +39,31 @@
             imageData[start + 4*i]    = pallete[rand];
             imageData[1+ start + 4*i] = pallete[rand+1];
             imageData[2+ start + 4*i] = pallete[rand+2];
-            imageData[3+ start + 4*i] = 255;
+            imageData[3+ start + 4*i] = 0xFF;
         }
         
         /* Perform full generation */
         for(var y=0,start=0;y<canvas.height;y++,start=oneLine*y){
             for(var x=0,thisPixel=0;x<canvas.width;x++,thisPixel=start+4*x){
                 // Red
-                imageData[thisPixel] = ((imageData[thisPixel+oneLine] 
+                imageData[thisPixel] = ((
+                     imageData[thisPixel+oneLine] 
                     +imageData[thisPixel + oneLine +4] 
                     +imageData[thisPixel + oneLine -4] 
                     +imageData[thisPixel + oneLine *2] 
                 )*32)/120;
 
                 // Green
-                imageData[1+ thisPixel] = ((imageData[1+ thisPixel+oneLine] 
+                imageData[1+ thisPixel] = ((
+                     imageData[1+ thisPixel+oneLine] 
                     +imageData[1+ thisPixel + oneLine +4] 
                     +imageData[1+ thisPixel + oneLine -4] 
                     +imageData[1+ thisPixel + oneLine *2] 
                 )*32)/120;
 
                 // Blue
-                imageData[2+ thisPixel] = ((imageData[2+ thisPixel+oneLine] 
+                imageData[2+ thisPixel] = ((
+                     imageData[2+ thisPixel+oneLine] 
                     +imageData[2+ thisPixel + oneLine +4] 
                     +imageData[2+ thisPixel + oneLine -4] 
                     +imageData[2+ thisPixel + oneLine *2] 
@@ -110,7 +112,6 @@
         if (s==0){
             r = g = b = 1;
         } else {
-
             var q = l < 0.5 ? l * (1+s) : l+s -l*s;
             var p = 2 * l -q;
             r = hue2rgb(p,q,h+1/3);
